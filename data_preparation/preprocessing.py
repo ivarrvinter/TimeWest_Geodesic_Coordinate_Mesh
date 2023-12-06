8f19beff8975bec94fcf8d054e28b438d9a31287
Amirhossein Ghanipour <109881262+ivarrvinter@users.noreply.github.com>
2023-12-06 05:08:25 -0800

    Updated the token and id conversion

--- data_preparation/preprocessing.py
+++ data_preparation/preprocessing.py
@@ -3,19 +3,24 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
 class Preprocessor:
-    @staticmethod
-    def apply_substitution(text: str, rule):
+    def __init__(self, vocab_mapper):
+        self.vocab_mapper = vocab_mapper
+
+    def apply_substitution(self, text: str, rule):
         return re.sub(rule, '', text) if text else None
     
-    @staticmethod
-    def convert_to_lower(text: str):
+    def convert_to_lower(self, text: str):
         return text.lower() if text else None
     
-    @staticmethod
-    def tokenize_text(text: str):
+    def tokenize_text(self, text: str):
         return word_tokenize(text) if text else []
     
-    @staticmethod
-    def lemmatize_text(tokens):
+    def lemmatize_text(self, tokens):
         lemmatizer = WordNetLemmatizer()
         return [lemmatizer.lemmatize(token) for token in tokens]
+    
+    def convert_tokens_to_ids(self, tokens):
+        return self.vocab_mapper.convert_tokens_to_ids(tokens)
+    
+    def convert_ids_to_tokens(self, ids):
+        return self.vocab_mapper.convert_ids_to_tokens(ids)
