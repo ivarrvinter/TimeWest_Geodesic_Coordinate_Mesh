bab2def9b9f299fac3bcb3e4f7098ffc306fa783
Amirhossein Ghanipour <109881262+ivarrvinter@users.noreply.github.com>
2023-12-06 07:03:25 -0800

    Update tokens

--- data_preparation/preprocessing.py
+++ data_preparation/preprocessing.py
@@ -1,10 +1,10 @@
 import re
 from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
 
 class Preprocessor:
     def __init__(self, vocab_mapper):
         self.vocab_mapper = vocab_mapper
+        self.special_tokens = vocab_mapper.special_tokens
 
     def apply_substitution(self, text: str, rule):
         return re.sub(rule, '', text) if text else None
@@ -13,14 +13,13 @@ class Preprocessor:
         return text.lower() if text else None
     
     def tokenize_text(self, text: str):
-        return word_tokenize(text) if text else []
-    
-    def lemmatize_text(self, tokens):
-        lemmatizer = WordNetLemmatizer()
-        return [lemmatizer.lemmatize(token) for token in tokens]
-    
+        tokens = word_tokenize(text) if text else []
+        tokens = [self.special_tokens.get(token, token) for token in tokens]
+        return tokens
+
     def convert_tokens_to_ids(self, tokens):
         return self.vocab_mapper.convert_tokens_to_ids(tokens)
     
     def convert_ids_to_tokens(self, ids):
         return self.vocab_mapper.convert_ids_to_tokens(ids)
+    
