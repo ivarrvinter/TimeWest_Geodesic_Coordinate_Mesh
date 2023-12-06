fb0cf1d0c6a0cae05864db94d51a58c93de71ee5
Amirhossein Ghanipour <109881262+ivarrvinter@users.noreply.github.com>
2023-12-06 05:09:21 -0800

    Added token to ids and vice versa conversion

--- mapper/term_vocab_mapper.py
+++ mapper/term_vocab_mapper.py
@@ -1,15 +1,26 @@
-class TermVocabMapper():
-    @staticmethod
-    def map_terms_to_vocab(text):
-        word_to_index = {}
-        index_to_word = {}
-        
-        index = 0
+class TermVocabMapper:
+    def __init__(self):
+        self.word_to_index = {}
+        self.index_to_word = {}
+        self.unk_token = '[UNK]'
+        self.add_special_tokens()
 
-        for word in text:
-            if word not in word_to_index:
-                word_to_index[word] = index
-                index_to_word[index] = word
+    def add_special_tokens(self):
+        self.word_to_index[self.unk_token] = 0
+        self.index_to_word[0] = self.unk_token
+
+    def map_terms_to_vocab(self, tokens):
+        index = len(self.word_to_index)
+
+        for token in tokens:
+            if token not in self.word_to_index:
+                self.word_to_index[token] = index
+                self.index_to_word[index] = token
                 index += 1
-        
-        return word_to_index, index_to_word
+
+    def convert_tokens_to_ids(self, tokens):
+        return [self.word_to_index.get(token, 0) for token in tokens]
+    
+    def convert_ids_to_tokens(self, ids):
+        return [self.index_to_word.get(i, self.unk_token) for i in ids]
+    
